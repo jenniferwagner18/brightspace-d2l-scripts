62dbb88b6d8a33dd7e8acee89147513e6d2bcc7a
Jennifer Wagner <49212830+jenniferwagner18@users.noreply.github.com>
2021-12-05 12:19:18 -0500

    Update d2l-survey-pivot.py

--- d2l-survey-pivot.py
+++ d2l-survey-pivot.py
@@ -1,16 +1,17 @@
 # Pivots individual attempts survey report from D2L so that Likert questions are in columns instead of rows
-# Update paths to where your CSV file is located on your computer and where you want to save the new file
 
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('C:/Users/username/Desktop/survey-report.csv')
-df = df.iloc[:, [0,7,8,9]] # use only four columns in dataframe
+df = pd.read_csv('survey-report.csv')
+
+df = df.iloc[:, [0,7,8,9]]
 df.columns = ['Students', 'Questions', 'Answers', 'Num_Responses']
-df = df[df.Num_Responses != 0.0] # remove rows where response is 0
-df[df.Students == ""] = np.NaN # replace empty cells with NaN
-df.Students = df.Students.ffill() # flash fill students' names to replace NaN
-df.dropna(subset=['Questions'], inplace=True) # remove rows with no questions/answers
+df = df[df.Num_Responses != 0.0]
+df[df.Students == ""] = np.NaN
+df.Students = df.Students.ffill()
+df.dropna(subset=['Questions'], inplace=True)
+
 table = pd.pivot_table(df, values='Answers', index='Students', columns='Questions', aggfunc=np.mean)
 
-table.to_csv('C:/Users/username/Desktop/survey-pivoted.csv')
+table.to_csv('survey-pivoted.csv')
