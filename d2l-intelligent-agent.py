ef9d8909d9ee9cd21b1bf1ab2ea64a558283102b
Jennifer Wagner <49212830+jenniferwagner18@users.noreply.github.com>
2021-12-08 14:58:30 -0500

    Update d2l-intelligent-agent.py

--- d2l-intelligent-agent.py
+++ d2l-intelligent-agent.py
@@ -1,6 +1,8 @@
 # This script merges the classlist exported from D2L gradebook with the export history of an   
 # intelligent agent that returns students who have earned the award (based on release condition)
-# and formats the email addresses properly even if guest accounts with various domains are used
+# and formats the email addresses properly even if guest accounts with various domains are used.
+
+# Replace @yourschool.edu with your institution's domain.
 
 import pandas as pd
 df1 = pd.read_csv('classlist.csv')
@@ -15,6 +17,6 @@ df['EarnedAward'].fillna('False', inplace=True)
 df['EarnedAward'].replace(r'^2.*Z$', 'True', regex=True, inplace=True)
 
 df['Email'].replace(r'_at_', '@', regex=True, inplace=True)
-df['Email'] = df['Email'].where(df['Email'].str.contains('@'), df['Email'].astype(str) + '@msu.edu')
+df['Email'] = df['Email'].where(df['Email'].str.contains('@'), df['Email'].astype(str) + '@yourschool.edu')
 
 df.to_csv('awards-earned.csv', columns=['LastName', 'FirstName', 'Email', 'EarnedAward'], index=False)
