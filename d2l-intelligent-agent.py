f1fc776355eb186afb4f37485d5eeb1cff5de4ff
Jennifer Wagner <49212830+jenniferwagner18@users.noreply.github.com>
2021-12-13 18:34:10 -0500

    Update d2l-intelligent-agent.py

--- d2l-intelligent-agent.py
+++ d2l-intelligent-agent.py
@@ -1,7 +1,7 @@
 # This script merges the classlist exported from D2L gradebook with the export history of an intelligent
 # agent and formats the email addresses properly even if guest accounts with various domains are used.
 
-# Replace @yourschool.edu with your institution's domain.
+# Don't forget to replace @yourschool.edu with your institution's domain.
 
 import pandas as pd
 df1 = pd.read_csv('classlist.csv')
@@ -11,8 +11,6 @@ df = pd.merge(df1,df2,on=['LastName', 'FirstName'], how='left')
 df = df.drop_duplicates(subset=['Email'])
 
 df['Identified'] = df['AgentName'].notnull()
-df = df[['LastName', 'FirstName', 'Email', 'Identified']]
-
 df['Email'].replace(r'_at_', '@', regex=True, inplace=True)
 df['Email'] = df['Email'].where(df['Email'].str.contains('@'), df['Email'].astype(str) + '@yourschool.edu')
 
