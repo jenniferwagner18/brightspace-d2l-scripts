92aa703d15a3113e63ef4a9c1e9be3cc4bf4978f
Jennifer Wagner <49212830+jenniferwagner18@users.noreply.github.com>
2021-12-13 18:32:28 -0500

    Update d2l-intelligent-agent.py

--- d2l-intelligent-agent.py
+++ d2l-intelligent-agent.py
@@ -9,12 +9,11 @@ df1.columns = df1.columns.str.replace(' ', '')
 df2 = pd.read_csv('agent.csv')
 df = pd.merge(df1,df2,on=['LastName', 'FirstName'], how='left')
 df = df.drop_duplicates(subset=['Email'])
-df.rename(columns={'AgentName': 'InAgentReport'}, inplace=True)
 
-df['InAgentReport'].replace(r'^.+$', 'True', regex=True, inplace=True)
-df['InAgentReport'].fillna('False', inplace=True)
+df['Identified'] = df['AgentName'].notnull()
+df = df[['LastName', 'FirstName', 'Email', 'Identified']]
 
 df['Email'].replace(r'_at_', '@', regex=True, inplace=True)
 df['Email'] = df['Email'].where(df['Email'].str.contains('@'), df['Email'].astype(str) + '@yourschool.edu')
 
-df.to_csv('agent-final.csv', columns=['LastName', 'FirstName', 'Email', 'InAgentReport'], index=False)
+df.to_csv('agent-final.csv', columns=['LastName', 'FirstName', 'Email', 'Identified'], index=False)
