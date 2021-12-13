cc962cfefed1d8190b9bcfd19063067786a16a03
Jennifer Wagner <49212830+jenniferwagner18@users.noreply.github.com>
2021-12-13 16:23:12 -0500

    Update d2l-intelligent-agent.py
    Updated code so it can be used with any intelligent agent

--- d2l-intelligent-agent.py
+++ d2l-intelligent-agent.py
@@ -1,6 +1,5 @@
-# This script merges the classlist exported from D2L gradebook with the export history of an   
-# intelligent agent that returns students who have earned the award (based on release condition)
-# and formats the email addresses properly even if guest accounts with various domains are used.
+# This script merges the classlist exported from D2L gradebook with the export history of an intelligent
+# agent and formats the email addresses properly even if guest accounts with various domains are used.
 
 # Replace @yourschool.edu with your institution's domain.
 
@@ -10,13 +9,12 @@ df1.columns = df1.columns.str.replace(' ', '')
 df2 = pd.read_csv('agent.csv')
 df = pd.merge(df1,df2,on=['LastName', 'FirstName'], how='left')
 df = df.drop_duplicates(subset=['Email'])
-df.rename(columns={'AgentRunDate': 'EarnedAward'}, inplace=True)
-df = df[['LastName', 'FirstName', 'Email', 'EarnedAward']]
+df.rename(columns={'AgentName': 'InAgentReport'}, inplace=True)
 
-df['EarnedAward'].fillna('False', inplace=True)
-df['EarnedAward'].replace(r'^2.*Z$', 'True', regex=True, inplace=True)
+df['InAgentReport'].replace(r'^.+$', 'True', regex=True, inplace=True)
+df['InAgentReport'].fillna('False', inplace=True)
 
 df['Email'].replace(r'_at_', '@', regex=True, inplace=True)
 df['Email'] = df['Email'].where(df['Email'].str.contains('@'), df['Email'].astype(str) + '@yourschool.edu')
 
-df.to_csv('awards-earned.csv', columns=['LastName', 'FirstName', 'Email', 'EarnedAward'], index=False)
+df.to_csv('awards-earned.csv', columns=['LastName', 'FirstName', 'Email', 'InAgentReport'], index=False)
